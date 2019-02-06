852d77ae5c36476105dd5d3f8c91e6f28e2ccd48
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-02-06 17:37:31 +0100

    Add cors to server, add dummy api endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,24 @@
 import express from 'express'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 
 const app = express()
 const port = process.env.PORT || 5656
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/api/stuff', (req, res) => {
   console.log(req.body.post)
 
   res.send({ express: `Hello From Express, you sent me: ${req.body.post}` })
+})
+
+app.get('/api/v1/tacos', (req, res) => {
+  console.log(req.body.post)
+
+  res.send({ })
 })
 
 app.listen(port, () => {
