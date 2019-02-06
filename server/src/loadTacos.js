71f146e61f3b18f4230d45d853229b75ceb4a0a5
Daniel Arrizza <daniel.arrizza@appdynamics.com>
2019-02-06 12:48:41 -0800

    add tacoDetails sample data to loadTacos

--- server/src/loadTacos.js
+++ server/src/loadTacos.js
@@ -2,9 +2,66 @@ export default () => {
   console.log('loading tacos')
 
   const sampleList = [
-    { id: '1', name: 'foo', description: 'lorem ipsum sit dolor' },
-    { id: '2', name: 'bar', description: 'lorem ipsum sit dolor' },
-    { id: '3', name: 'baz', description: 'lorem ipsum sit dolor' },
+    {
+      id: '1',
+      name: 'foo',
+      description: 'lorem ipsum sit dolor',
+      version: '1.3',
+      tags: ['java', 'spring'],
+      authors: 'Einstein',
+      ingredients: [
+        {
+          type: 'btRules',
+          contents: 'JSON from Config exporter',
+          doc: 'This is a good config',
+        },
+        {
+          type: 'eum',
+          contents: 'JSON from Config exporter',
+          doc: 'This is the best config',
+        },
+      ],
+    },
+    {
+      id: '2',
+      name: 'bar',
+      description: 'lorem ipsum sit dolor',
+      version: '1.3',
+      tags: ['java', 'spring'],
+      authors: 'Einstein',
+      ingredients: [
+        {
+          type: 'btRules',
+          contents: 'JSON from Config exporter',
+          doc: 'This is a good config',
+        },
+        {
+          type: 'eum',
+          contents: 'JSON from Config exporter',
+          doc: 'This is the best config',
+        },
+      ],
+    },
+    {
+      id: '3',
+      name: 'baz',
+      description: 'lorem ipsum sit dolor',
+      version: '1.3',
+      tags: ['java', 'spring'],
+      authors: 'Einstein',
+      ingredients: [
+        {
+          type: 'btRules',
+          contents: 'JSON from Config exporter',
+          doc: 'This is a good config',
+        },
+        {
+          type: 'eum',
+          contents: 'JSON from Config exporter',
+          doc: 'This is the best config',
+        },
+      ],
+    },
   ]
 
   return sampleList
